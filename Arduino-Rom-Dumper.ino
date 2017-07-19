b13a3cbdd12a71fffe7d8450c408eb19ad08e289
SukkoPera <software@sukkology.net>
2017-07-19 23:29:53 +0200

    Initial commit

---
+++ Arduino-Rom-Dumper.ino
@@ -0,0 +1,72 @@
+/* Arduino pins 3/4/5/6/7 = ROM D3/D4/D5/D6/D7
+ * Arduino pins 8/9/10 = ROM D0/D1/D2 
+ */
+
+const unsigned long SERIAL_SPEED = 9600;
+
+const word ROM_SIZE = 4 * 1024;
+
+// Delay between address write and data read (us)
+const byte READ_DELAY = 50;
+
+const byte SR_LATCH_PIN = A0;
+const byte SR_DATA_PIN = A1;
+const byte SR_CLOCK_PIN = A2;
+
+void write_address (word address) {
+  digitalWrite (SR_LATCH_PIN, LOW);
+  shiftOut (SR_DATA_PIN, SR_CLOCK_PIN, MSBFIRST, address >> 8);
+  shiftOut (SR_DATA_PIN, SR_CLOCK_PIN, MSBFIRST, address & 0xFF);
+  digitalWrite (SR_LATCH_PIN, HIGH);
+}
+
+byte read_byte (word address) {
+  write_address (address);
+  delayMicroseconds (READ_DELAY);
+  byte data = (PIND & 0xF8) | (PINB & 0x07);
+
+  return data;
+}
+
+void setup () {
+  // Data lines
+  pinMode (3, INPUT);
+  pinMode (4, INPUT);
+  pinMode (5, INPUT);
+  pinMode (6, INPUT);
+  pinMode (7, INPUT);
+  pinMode (8, INPUT);
+  pinMode (9, INPUT);
+  pinMode (10, INPUT);
+
+  // SR control lines
+  pinMode (SR_LATCH_PIN,  OUTPUT);
+  pinMode (SR_DATA_PIN,  OUTPUT);
+  pinMode (SR_CLOCK_PIN,  OUTPUT);
+  digitalWrite (SR_LATCH_PIN, HIGH);
+
+  // Pin
+  pinMode (LED_BUILTIN, OUTPUT);
+
+  // Serial port for I/O
+  Serial.begin (SERIAL_SPEED);
+}
+
+void loop () {
+  // Wait for a character on serial port
+  bool start = false;
+  while (!start) {
+    digitalWrite (LED_BUILTIN, (millis () / 333) % 2);
+    while (Serial.available ()) {
+      Serial.read ();
+      start = true;
+    }
+  }
+
+  // Go!
+  for (word i = 0; i < ROM_SIZE; i++) {
+    byte b = read_byte (i);
+    Serial.write (b);
+  }
+}
+
---
+++ tools/dump.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import time
+import serial
+
+LOGFILE = "dump.bin"
+SERIAL = "/dev/ttyUSB0"
+SPEED = 9600
+TIMEOUT = 1000	# ms
+BUFSIZE = 128
+
+with serial.Serial (SERIAL, SPEED, timeout = TIMEOUT / 1000.0) as ser, open (LOGFILE, "w") as fp:
+	# Wait for Arduino to settle down after reset consequent to serial port opening
+	time.sleep (3)
+
+	# Tell sketch to start dumping (Any char will do)
+	ser.write ("A");
+
+	recv = 0;
+	while True:
+		buf = ser.read (BUFSIZE)
+		if buf:
+			recv += len (buf)
+			fp.write (buf)
+			print recv
+		else:
+			print "<TIMEOUT>"
+			break
